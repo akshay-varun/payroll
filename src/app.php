27c3d9c959da818a0a904a656f90c3e1e17b33b6
akshay-varun <akshaygr4@gmail.com>
2019-11-12 20:19:56 +0530

    Done with adding company

--- src/app.php
+++ src/app.php
@@ -12,11 +12,33 @@ $twig = new Environment($loader);
 
 
 try {
-    $router->addMatch('GET', '/', function () {
-        global $twig;
+    $router->addMatch('GET', '/', function () use($twig) {
         echo $twig->render('default.twig', array('title' => 'EasyPHP-boilerplate'));
     });
 
+    $router->addMatch('GET', '/company', function () use($twig) {
+        echo $twig->render('company.twig');
+    });
+
+    $router->addMatch('GET', '/admin_login',function () use($twig){
+        echo $twig->render('admin_login.twig');
+    });
+
+    $router->addMatch('GET','/loginbtn',function () use($twig){
+        echo $twig->render('loginbtn.twig');
+    });
+
+    $router->addMatch('GET','/main_add',function () use($twig){
+        echo $twig->render('main_add.twig');
+    });
+
+    $router->addMatch('GET','/add_company',function () use($twig){
+        echo $twig->render('add_company.twig');
+    });
+
+    $router->addMatch('GET','/add_admin',function () use($twig){
+        echo $twig->render('add_admin.twig');
+    });
     $router->execute();
 
 } catch (Exception $e) {
---
+++ src/includes/db.php
@@ -0,0 +1,5 @@
+<php
+    define('DB_SERVER', 'localhost');
+    define('DB_USER', 'payroll_user');
+    define('DB_PASSWORD', 'Aksh!rthmcjjdskm8');
+    define('DB_NAME', 'payroll');
