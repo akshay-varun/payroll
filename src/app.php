3918ae03051f2f8e6429186e1cb131be5667831d
akshay-varun <akshaygr4@gmail.com>
2019-11-16 18:58:37 +0530

    Done with UI of Employee Add.

--- src/app.php
+++ src/app.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
 //require_once __DIR__ . "/logger.php";
 
 
@@ -18,27 +19,27 @@ $twig = new Environment($loader);
 
 
 try {
-    $router->addMatch('GET', '/', function () use($twig) {
+    $router->addMatch('GET', '/', function () use ($twig) {
         echo $twig->render('default.twig', array('title' => 'EasyPHP-boilerplate'));
     });
 
-    $router->addMatch('GET', '/company', function () use($twig) {
+    $router->addMatch('GET', '/company', function () use ($twig) {
         echo $twig->render('company.twig');
     });
 
-    $router->addMatch('GET', '/admin_login',function () use($twig){
+    $router->addMatch('GET', '/admin_login', function () use ($twig) {
         echo $twig->render('admin_login.twig');
     });
 
-    $router->addMatch('GET','/loginbtn',function () use($twig){
+    $router->addMatch('GET', '/loginbtn', function () use ($twig) {
         echo $twig->render('loginbtn.twig');
     });
 
-    $router->addMatch('GET','/main_add',function () use($twig){
+    $router->addMatch('GET', '/main_add', function () use ($twig) {
         echo $twig->render('main_add.twig');
     });
 
-    $router->addMatch('GET','/add_company',function () use($twig){
+    $router->addMatch('GET', '/add_company', function () use ($twig) {
         echo $twig->render('add_company.twig');
     });
 
@@ -53,8 +54,8 @@ try {
 
             $val['name'] = $name;
 
-                $company= new add_company();
-                $company->cmp_Name($name);
+            $company = new add_company();
+            $company->cmp_Name($name);
 
 
         } else {
@@ -64,49 +65,72 @@ try {
 
     });
 
-    $router->addMatch('GET','/add_admin',function () use($twig){
+    $router->addMatch('GET', '/add_admin', function () use ($twig) {
         echo $twig->render('add_admin.twig');
     });
 
-    $router->addMatch('POST', '/add_admin', function ()
-    {
+    $router->addMatch('POST', '/add_admin', function () {
         global $twig;
-        $val=array();
-        $error=array();
+        $val = array();
+        $error = array();
 
-        if(isset($_POST['Admin_Name']) && isset($_POST['Email']) && isset($_POST['Company_ID']) && isset($_POST['Password']))
-        {
-            $Admin_Name=Functions::escapeInput($_POST['Admin_Name']);
-            $Email=Functions::escapeInput($_POST['Email']);
-            $Password=Functions::escapeInput($_POST['Password']);
-            $Company_ID=Functions::escapeInput($_POST['Company_ID']);
+        if (isset($_POST['Admin_Name']) && isset($_POST['Email']) && isset($_POST['Company_ID']) && isset($_POST['Password'])) {
+            $Admin_Name = Functions::escapeInput($_POST['Admin_Name']);
+            $Email = Functions::escapeInput($_POST['Email']);
+            $Password = Functions::escapeInput($_POST['Password']);
+            $Company_ID = Functions::escapeInput($_POST['Company_ID']);
 
-            $val['Admin_Name']=$Admin_Name;
-            $val['Email']=$Email;
-            $val['Password']=$Password;
-            $val['Company_ID']=$Company_ID;
+            $val['Admin_Name'] = $Admin_Name;
+            $val['Email'] = $Email;
+            $val['Password'] = $Password;
+            $val['Company_ID'] = $Company_ID;
 
-            $admin=new add_admin();
+            $admin = new add_admin();
             $admin->adminADD($Admin_Name, $Email, $Password, $Company_ID);
 
 
-            }
-        else
-        {
+        } else {
+            $error['general'] = "Fill all the fields.";
+        }
+        EasyHeaders::redirect('/main_add');
+    });
+
+    $router->addMatch('POST', '/add_Employee', function () {
+        global $twig;
+        $val = array();
+        $error = array();
+
+        if (isset($_POST['Emp_Name']) && isset($_POST['Emp_Email']) && isset($_POST['Emp_Phone']) && isset($_POST['Admin_ID']) && isset($_POST['Salary'])) {
+            $Emp_Name = Functions::escapeInput($_POST['Emp_Name']);
+            $Emp_Email = Functions::escapeInput($_POST['Emp_Email']);
+            $Emp_Phone = Functions::escapeInput($_POST['Emp_Phone']);
+            $Admin_ID = Functions::escapeInput($_POST['Admin_ID']);
+            $Salary=Functions::escapeInput($_POST['Salary']);
+
+            $val['Emp_Name'] = $Emp_Name;
+            $val['Emp_Email'] = $Emp_Email;
+            $val['Emp_Phone'] = $Emp_Phone;
+            $val['Admin_ID'] = $Admin_ID;
+            $val['Salary']=$Salary;
+
+            $employee = new add_Employee();
+            $employee->EmployeeADD($Emp_Name,$Emp_Email,$Emp_Phone,$Admin_ID,$Salary);
+
+
+        } else {
             $error['general'] = "Fill all the fields.";
         }
         EasyHeaders::redirect('/main_add');
     });
 
 
-    $router->addMatch('GET', '/dashboard', function () use($twig) {
+    $router->addMatch('GET', '/dashboard', function () use ($twig) {
         echo $twig->render('dashboard.twig');
     });
 
-    $router->addMatch('GET', '/add_Employee', function () use($twig) {
+    $router->addMatch('GET', '/add_Employee', function () use ($twig) {
         echo $twig->render('add_Employee.twig');
     });
-
 
 
     $router->execute();
